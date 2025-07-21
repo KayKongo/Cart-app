f40bd8ffa7074e6285d78af4b9ff617441d903a2
kaykongo <pkkongo@st.ug.edu.gh>
2025-07-21 22:30:24 +0000

    feat: implemented the clear cart, remove, increase, and a bit of decrease functionalities

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,13 +1,13 @@
-import { FaCartPlus } from 'react-icons/fa';
+import { FaCartPlus } from "react-icons/fa";
 const Navbar = () => {
   return (
     <nav>
-      <div className='nav-center'>
-        <h4>useReducer</h4>
-        <div className='nav-container'>
-          <FaCartPlus className='cart-icon' />
-          <div className='amount-container'>
-            <p className='total-amount'>2</p>
+      <div className="nav-center">
+        <h4 style={{ fontWeight: "bold", fontSize: "1.5rem" }}>Cart App</h4>
+        <div className="nav-container">
+          <FaCartPlus className="cart-icon" />
+          <div className="amount-container">
+            <p className="total-amount">2</p>
           </div>
         </div>
       </div>
---
+++ src/reducer.js
@@ -0,0 +1,46 @@
+import {
+  CLEAR_CART,
+  REMOVE,
+  INCREASE,
+  DECREASE,
+  LOADING,
+  DISPLAY_ITEMS,
+} from "./actions";
+
+const reducer = (state, action) => {
+  // Clear Cart Action
+  if (action.type === CLEAR_CART) {
+    return { ...state, cart: new Map() };
+  }
+
+  // Remove Item Action
+  if (action.type === REMOVE) {
+    const newCart = new Map(state.cart);
+    newCart.delete(action.payload.id);
+    return { ...state, cart: newCart };
+  }
+
+  // Increase Item Number Action
+  if (action.type === INCREASE) {
+    const newCart = new Map(state.cart);
+    const itemId = action.payload.id;
+    const item = newCart.get(itemId);
+    const newItem = { ...item, amount: item.amount + 1 };
+    newCart.set(itemId, newItem);
+    return { ...state, cart: newCart };
+  }
+
+  // Decrease Item Number Action
+  if (action.type === DECREASE) {
+    const newCart = new Map(state.cart);
+    const itemId = action.payload.id;
+    const item = newCart.get(itemId);
+    const newItem = { ...item, amount: item.amount - 1 };
+    newCart.set(itemId, newItem);
+    return { ...state, cart: newCart };
+  }
+
+  throw new Error(`no matching action type: ${action.type}`);
+};
+
+export default reducer;
